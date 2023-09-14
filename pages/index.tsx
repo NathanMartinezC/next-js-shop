2e083d62cc56544b99da73d53bb6b2e7df7eb113
Nathan Martinez <n.mtz.cas@gmail.com>
2023-09-13 20:23:00 -0600

    Init shop layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import type { NextPage } from "next";
+import { ShopLayout } from "@/components/layouts"; 
+import { Typography } from "@mui/material";
 
 const Home: NextPage = () => {
   return (
-    <h1>Next Shop</h1>
+    <ShopLayout title={'Next-Shop - Home'} pageDescription={'Best Next Shop'}>
+      <Typography variant={'h1'} component={'h1'}>Shop</Typography>
+      <Typography variant={'h2'} sx={{ mb: 1 }}>All products</Typography>
+    </ShopLayout>
   )
 }
 
