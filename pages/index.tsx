3d294340c8aa9a9fe4858c2ca730874314f7f669
Nathan Martinez <n.mtz.cas@gmail.com>
2023-09-13 21:18:13 -0600

    Added base home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,34 @@
 import type { NextPage } from "next";
 import { ShopLayout } from "@/components/layouts"; 
-import { Typography } from "@mui/material";
+import { Card, CardActionArea, CardMedia, Grid, Typography } from "@mui/material";
+import { initialData } from "@/database/products";
 
 const Home: NextPage = () => {
   return (
     <ShopLayout title={'Next-Shop - Home'} pageDescription={'Best Next Shop'}>
       <Typography variant={'h1'} component={'h1'}>Shop</Typography>
       <Typography variant={'h2'} sx={{ mb: 1 }}>All products</Typography>
+
+      <Grid container spacing={4}>
+        {
+          initialData.products.map( product => (
+            <Grid item xs={6} sm={4} key={ product.slug }>
+              <Card>
+                <CardActionArea>
+                  <CardMedia 
+                    component={'img'}
+                    image={`products/${product.images[0]}`}
+                    alt={product.title}
+                  />
+                </CardActionArea>
+              </Card>
+            </Grid>
+          ))
+        }
+
+      </Grid>
+
+
     </ShopLayout>
   )
 }
