9f4fe000dfc086891362bdff98e1fd141172d048
Nathan Martinez <n.mtz.cas@gmail.com>
2023-09-19 18:57:26 -0600

    Added filter to get_products endpoint

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,4 +1,4 @@
-import { db } from '@/database'
+import { db, SHOP_CONSTANTS } from '@/database'
 import { IProduct } from '@/interfaces'
 import { Product } from '@/models'
 import type { NextApiRequest, NextApiResponse } from 'next'
@@ -22,8 +22,17 @@ export default function handler(
 }
 
 const getProducts = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+
+    const { gender = 'all' } = req.query;
+
+    let condition = {};
+
+    if ( gender !== 'all' && SHOP_CONSTANTS.validGenders.includes(`${gender}`)) {
+        condition = { gender };
+    }
+
     await db.connect();
-    const products = await Product.find()
+    const products = await Product.find(condition)
         .select('title images price slug inStock -_id')
         .lean();
                                     
