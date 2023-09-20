8c68e8a6e0bc063edec133679172d4b87a5b1935
Nathan Martinez <n.mtz.cas@gmail.com>
2023-09-19 18:44:52 -0600

    Added ge_products endpoint

---
+++ pages/api/products/index.ts
@@ -0,0 +1,32 @@
+import { db } from '@/database'
+import { IProduct } from '@/interfaces'
+import { Product } from '@/models'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = 
+| { message: string }
+| IProduct[]
+
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+
+    switch (req.method) {
+        case 'GET':
+            return getProducts(req, res)
+        default:
+            return res.status(405).end(`Method ${req.method} Not Allowed`)
+    }
+
+}
+
+const getProducts = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+    await db.connect();
+    const products = await Product.find()
+        .select('title images price slug inStock -_id')
+        .lean();
+                                    
+    await db.disconnect();
+    return res.status(200).json(products)
+}
