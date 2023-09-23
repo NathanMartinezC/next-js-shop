e9aecd6b0cd904a90aa6d0e2f2efc7f6f28a4872
Nathan Martinez <n.mtz.cas@gmail.com>
2023-09-23 08:34:44 -0600

    Added User model

---
+++ models/User.ts
@@ -0,0 +1,23 @@
+import { IUser } from "@/interfaces";
+import mongoose, { Schema, model, Model } from "mongoose";
+
+const UserSchema: Schema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true},
+    password: { type: String, required: true },
+    role: {
+        type: String,
+        enum: {
+            values: ["admin", "client"],
+            message: "{VALUE} is not supported",
+            default: "client",
+            required: true,        
+        }
+    }
+}, {
+    timestamps: true,
+});
+
+export const User: Model<IUser> = mongoose.models.User || model('User', UserSchema);
+
+export default User;
