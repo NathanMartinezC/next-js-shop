8637788a92cd2f5d9656d22963ebd69abc421391
Nathan Martinez <n.mtz.cas@gmail.com>
2023-10-06 17:42:41 -0600

    Remove cookies on logout

--- context/auth/AuthProvider.tsx
+++ context/auth/AuthProvider.tsx
@@ -80,6 +80,14 @@ export const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
     const logout = () => {
         Cookies.remove('token');
         Cookies.remove('cart');
+        Cookies.remove('firstName');
+        Cookies.remove('lastName');
+        Cookies.remove('address');
+        Cookies.remove('address2');
+        Cookies.remove('postalCode');
+        Cookies.remove('city');
+        Cookies.remove('country');
+        Cookies.remove('phone');
         router.reload();
     }
 
