1b611701cc954cb3b85532a889bf157b61036912
Nathan Martinez <n.mtz.cas@gmail.com>
2023-10-09 21:48:10 -0600

    Added order model

---
+++ interfaces/order.ts
@@ -0,0 +1,38 @@
+import { IUser } from './';
+
+export interface IOrder {
+    _id?: string;
+    user?: IUser | string;
+    orderItems: IOrderItem[];
+    shippingAddress: IShippingAddress;
+    paymentResults?: string;
+
+    numberOfItems: number;
+    subTotal: number;
+    tax: number;
+    total: number;
+
+    isPaid?: boolean;
+    paidAt?: string;
+}
+
+export interface IOrderItem {
+    _id: string;
+    title: string;
+    size: string;
+    quantity: number;
+    slug: string;
+    image: string;
+    price: number;
+}
+
+export interface IShippingAddress {
+    firstName: string;
+    lastName: string;
+    address: string;
+    address2?: string;
+    postalCode: string;
+    city: string;
+    country: string;
+    phone: string;
+}
