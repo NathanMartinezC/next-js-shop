ba33cc5c0ff4b110887ba4ca93f5aba77746a80a
Nathan Martinez <n.mtz.cas@gmail.com>
2023-10-17 22:14:39 -0600

    Added dashboard main view

--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,15 +1,67 @@
 import React from 'react';
-import { DashboardOutlined } from '@mui/icons-material';
+import { AccessTimeOutlined, AttachMoneyOutlined, CancelPresentationOutlined, CategoryOutlined, CreditCardOutlined, DashboardOutlined, GroupOutlined, ProductionQuantityLimitsOutlined } from '@mui/icons-material';
 import { AdminLayout } from '@/components/layouts';
+import { Card, CardContent, Grid, Typography } from '@mui/material';
+import { SummaryTile } from '@/components/admin';
 
 const DashboardPage = () => {
     return (
         <AdminLayout 
             title="Dashboard" 
-            subTitle="Dashboard" 
+            subTitle="Summary of your store" 
             icon={<DashboardOutlined />}
         >
-            <h3>Dashboard</h3>
+            <Grid container spacing={2}>
+                <SummaryTile 
+                    title="0" 
+                    subTitle="Total Orders" 
+                    icon={<CreditCardOutlined color='secondary' sx={{ fontSize: 40 }}/>}
+                />
+
+                <SummaryTile 
+                    title="0" 
+                    subTitle="Paid Orders" 
+                    icon={<AttachMoneyOutlined color='success' sx={{ fontSize: 40 }}/>}
+                />
+
+                <SummaryTile 
+                    title="0" 
+                    subTitle="Pending Orders" 
+                    icon={<CreditCardOutlined color='error' sx={{ fontSize: 40 }}/>}
+                />
+
+                <SummaryTile 
+                    title="0" 
+                    subTitle="Clients" 
+                    icon={<GroupOutlined color='primary' sx={{ fontSize: 40 }}/>}
+                />
+
+                <SummaryTile 
+                    title="0" 
+                    subTitle="Products" 
+                    icon={<CategoryOutlined color='warning' sx={{ fontSize: 40 }}/>}
+                />
+
+                <SummaryTile 
+                    title="0" 
+                    subTitle="No Stock" 
+                    icon={<CancelPresentationOutlined color='error' sx={{ fontSize: 40 }}/>}
+                />
+
+                <SummaryTile 
+                    title="0" 
+                    subTitle="Low Stock" 
+                    icon={<ProductionQuantityLimitsOutlined color='warning' sx={{ fontSize: 40 }}/>}
+                />
+
+                <SummaryTile 
+                    title="0" 
+                    subTitle="Update: " 
+                    icon={<AccessTimeOutlined color='secondary' sx={{ fontSize: 40 }}/>}
+                />
+            </Grid>
+
+
         </AdminLayout>
     )
 }
